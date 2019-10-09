48b87f774edc1788b36c57a4ef43855c55365675
BorislavDev <56331227+BorislavDev@users.noreply.github.com>
2019-10-09 16:35:01 +0300

    cosmetics release

--- pz1/include/Student.h
+++ pz1/include/Student.h
@@ -8,32 +8,38 @@
 #include <fstream>
 #include <string>
 
+	//	The student has a name in the name and on attendance data
 class Student {
 private:	
-	std::string name;
-	static const unsigned Days = 10;
-	bool arr[Days];
-	Student *next;
+	std::string name;	//	student name
+	static const unsigned Days = 10;	//	total days	
+	bool arr[Days];	//	an array of visits for a certain number of days per student
+	Student *next;	//	pointer to the next item in a linked list
 public:
+	// next editor
 	void add_next(Student* head) {
 		next = head;
 	}
 
-
+	//	name editor
 	void edit_name(std::string& str){
  		name = str;
 	}
+	//	Access to next
  	Student* get_next()
  	{ return next; }
 
+ 	// Access to name
  	std::string get_name()
  	{ return name;}
 
+ 	//	filling in visits
  	void note(bool y, unsigned day){
  		if(day<Days)
  		arr[day] = y;
 
  	}
+ 	//	student attendance output
  	void write_arr_bool(std::ofstream& file, unsigned day){
  		for(unsigned i = 0; i < day; i++)
  			file << ' ' << arr[i];
